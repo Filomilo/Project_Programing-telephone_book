059d4fee55358b296137738afa0faeb1f8dafc39
MaciejC1 <100415822+MaciejC1@users.noreply.github.com>
2022-04-05 17:20:39 +0200

    Update phone_book.c

--- Project_programing/Project_programing/phone_book.c
+++ Project_programing/Project_programing/phone_book.c
@@ -38,7 +38,7 @@ void print_contact(contact_type contact)
 	printf("ID: %d\n", contact.id);
 	printf("Name: %s\tSurname: %s\n", contact.name,contact.surname);
 	printf("%s, st.%s %d     %s\n", contact.adress.city, contact.adress.street, contact.adress.number, contact.adress.postal_code);
-	printf("pohone number: %s", contact.phone_number);
+	printf("Phone number: %s", contact.phone_number);
 	printf("\n\n\n");
 }
 

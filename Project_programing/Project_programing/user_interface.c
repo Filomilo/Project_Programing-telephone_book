4eac21a67936a231c6107b38d6bc68557076c42c
Maciej Cedzyński <100415822+MaciejC1@users.noreply.github.com>
2022-05-03 16:44:25 +0200

    poprawione bledy

--- Project_programing/Project_programing/user_interface.c
+++ Project_programing/Project_programing/user_interface.c
@@ -1,7 +1,7 @@
 /**
  * @file user_interface.c
  * @author Tomasz Bogdan, Maciej Cedzyński, Filip Borowiec
- * @brief plik zawiera funkcje odpowiedzialne konsolowy interfejs użytkownika
+ * @brief plik zawiera funkcje odpowiedzialne za konsolowy interfejs użytkownika
  * @version 0.1
  * @date 2022-03-24
  * 
@@ -25,11 +25,11 @@
 
 
  /**
-  * @brief funkcja odpowiedzialna za wpisywanie danych do dowanie nowego elementu do listy o podanych przez użytkownika danych
+  * @brief funkcja odpowiedzialna za wpisywanie nowego elementu do listy o podanych przez użytkownika danych
   *
-  * funkcja tworzy nowy element contact type a następnie po kolei prosi użytkownika o podanie
-  * imienia i nazwiska, nazwy ulicy, numeru domu/budynku, kodu pocztowego, nazwy miasta, numer telefonu
-  * następnie elemnt_conatact_type zostaje dodany do listy z użyciem funkcji list_add_head
+  * funkcja tworzy nowy element contact_type a następnie po kolei prosi użytkownika o podanie:
+  * imienia i nazwiska, nazwy ulicy, numeru domu/budynku, kodu pocztowego, nazwy miasta, numer telefonu,
+  * następnie element_contact_type zostaje dodany do listy z użyciem funkcji list_add_head
   *
   * @param list - struktura list_pointers określająca listę do której użytkownik ma dopisać element
   * @return list_pointers - zwraca struktura lsit_pointers z dodanym elementem przez użytkownika
@@ -43,23 +43,23 @@ list_pointers add_new_element(list_pointers list)
 	system("cls");
 	contact_type contact;
 	
-	printf("please provide full name of your contact:\n");
+	printf("Please provide full name of your contact:\n");
 	scanf_s("%s", contact.name, 30);
 	scanf_s("%s", contact.surname, 30);
 
-	printf("please provide street name of your contact:\n");
+	printf("Please provide street name of your contact:\n");
 	scanf_s("%s", contact.adress.street, 30);
 
-	printf("please provide house number of your contact:\n");
+	printf("Please provide house number of your contact:\n");
 	scanf_s("%d", &contact.adress.number, 1);
 
-	printf("please provide postal code of your contact:\n");
+	printf("Please provide postal code of your contact:\n");
 	scanf_s("%s", contact.adress.postal_code, 7);
 
-	printf("please provide city name of your contact:\n");
+	printf("Please provide city name of your contact:\n");
 	scanf_s("%s", contact.adress.city, 30);
 
-	printf("please provide phone number of your contact:\n");
+	printf("Please provide phone number of your contact:\n");
 	scanf_s("%s", contact.phone_number, 10);
 	 
 	contact.id = generate_id(list);
@@ -74,12 +74,12 @@ list_pointers add_new_element(list_pointers list)
 /**
  * @brief funkcja odpowiada za interfejs pozwalający przeszukać listę
  *
- * funkcja wpierw w porodzi użytkownika o podanie ciągu znaków według której ma zostać przeszukana lista
- * a następnie tworzy listę rezultatów z użyciem tego ciągu znaków oraz funkcji podanej jako argument funkcji określająca
- * według którego elementu jest przeszukiwane
+ * funkcja najpierw prosi użytkownika o podanie ciągu znaków według której ma zostać przeszukana lista,
+ * a następnie tworzy listę rezultatów z użyciem tego ciągu znaków oraz funkcji podanej jako argument funkcji określający
+ * według którego elementu jest przeszukiwana
  *
  * @param list - struktura list_pointers określająca listę która użytkownik przeszukuje
- * @param search_option - wskaźnik na funkcje określające według którego elementu kontaktu będzie przeszukiwanie można wybrać:
+ * @param search_option - wskaźnik na funkcje określający według którego elementu kontaktu będzie można wybrać przeszukiwanie:
  * is_search_city, is_search_name, is_search_number,  is_search_phone_number, is_search_postal_code, is_search_street, is_search_surname
  */
 
@@ -93,7 +93,7 @@ void searching(list_pointers list, int(*search_option)(char[30]))
 	result = search_results(list, search_option, text);
 	print_list_head(result);
 	free_list(result);
-	printf("push eneter to return\n");
+	printf("Push eneter to return\n");
 	getchar();
 	getchar();
 
@@ -107,8 +107,8 @@ void searching(list_pointers list, int(*search_option)(char[30]))
 /**
  * @brief funkcja generująca interfejs wyboru elementów do przeszukania
  *
- * funkcja ta na początku wyświetla opcje wyboru od 0 do 7 i pobiera od użytkownika wartość wyboru
- * w zależności od wyboru funkcja później przechodzi do do funkcji searching z odpowiednio wybrana funkcja "is_searching_******"
+ * funkcja ta na początku wyświetla opcje wyboru od 0 do 7 i pobiera od użytkownika wartość wyboru,
+ * w zależności od wyboru funkcja później przechodzi do funkcji searching z odpowiednio wybrana funkcja "is_searching_******"
  *
  *
  * @param list - struktura list_pointers określająca listę która użytkownik przeszukuje
@@ -121,7 +121,7 @@ void search_phone_book(list_pointers list)
 	{
 		system("cls");
 		printf("Depending on what parameter, would you like to search: \n\n");
-			printf("7- name\n6- surname\n5- phone number\n4- city \n3- street\n2- postal code\n1- house number\n0-fisnish searching phone book\n\n ");
+			printf("7- name\n6- surname\n5- phone number\n4- city \n3- street\n2- postal code\n1- house number\n0- finish searching phone book\n\n ");
 		scanf_s("%d", &choice, 1);
 		switch (choice)
 		{
@@ -146,23 +146,23 @@ void search_phone_book(list_pointers list)
 /**
  * @brief funkcja generuje interfejs umożliwiający sortowanie listy
  *
- * funckja na początku wyświetla użytkownika opcje wyboru według którego elementu chce użytkownik posortować listę
- * w zależności od wyboru użytkownika uruchamia się funkcja merge_sort ze wskaźnikiem na na funkcje "****_cmp"
+ * funkcja na początku wyświetla uzytkownikowi opcje wyboru według którego elementu użytkownik chce posortować listę,
+ * w zależności od wyboru użytkownika uruchamia się funkcja merge_sort ze wskaźnikiem na funkcje "****_cmp",
  * na koniec zwraca posortowana listę do głównego programu
  *
- * @param list -  struktura list_pointers okreslajaca listę która użytkownik che posortować
+ * @param list -  struktura list_pointers okreslajaca listę która użytkownik chce posortować
  * @return list_pointers -  struktura list_pointers określająca posortowaną listę w zależności od wyboru użytkownika
  */
 
 /**
  * @brief funkcja generuje interfejs umożliwiający sortowanie listy
  *
- * funckja na początku wyświetla użytkownika opcje wyboru według którego elementu chce użytkownik posortować listę
- * w zależności od wyboru użytkownika uruchamia się funkcja merge_sort ze wskaźnikiem na na funkcje "****_cmp"
+ * funkcja na początku wyświetla użytkownikowi opcje wyboru według którego elementu użytkownik chce posortować listę,
+ * w zależności od wyboru użytkownika uruchamia się funkcja merge_sort ze wskaźnikiem na funkcje "****_cmp",
  * na koniec zwraca posortowana listę do głównego programu
  *
  * @param list -  struktura list_pointers okreslajaca listę która użytkownik che posortować
- * @param order - wskaźnik na zmienna order określajaća w jakim kierunku ma się wyświetlać lista
+ * @param order - wskaźnik na zmienna order określajaca w jakim kierunku ma się wyświetlać lista
  * @return list_pointers -  struktura list_pointers określająca posortowaną listę w zależności od wyboru użytkownika
  */
 list_pointers sort_phone_book(list_pointers list, int *order)
@@ -176,12 +176,12 @@ list_pointers sort_phone_book(list_pointers list, int *order)
 		else
 			print_list_head(list);
 		printf("Depending on what parameter, would you like to sort: \n\n");
-		printf("7- id\n6- full name\n5- phone number\n4- city \n3- street\n2- postal code\n1- house number\n0-fisnish soting phone book\n\n ");
+		printf("7- id\n6- full name\n5- phone number\n4- city \n3- street\n2- postal code\n1- house number\n0- finish sorting phone book\n\n ");
 		scanf_s("%d", &choice, 1);
 		if (choice != 0)
 		{
 			printf("In what order would you like to sort: \n\n");
-			printf("1- descending \n0-Ascending\n\n ");
+			printf("1- Descending \n0- Ascending\n\n ");
 			scanf_s("%d", order, 1);
 		}
 
@@ -209,11 +209,11 @@ list_pointers sort_phone_book(list_pointers list, int *order)
 /**
  * @brief funkcja generująca interfejs umożliwiający usunięcie wybranego przez użytkownika elementu
  *
- * na samym początku funkcja prosi użytkownika o podanie id elementu który chce usunąć
+ * na samym początku funkcja prosi użytkownika o podanie id elementu który chce usunąć,
  * następnie przeszukuje listę w poszukiwaniu elementu o tym id a następnie go usuwa
  *
- * @param list  struktura list_pointers określająca listę z której użytkownika ma usunąć element
- * @param order - zmienna order określajaća w jakim kierunku ma się wyświetlać lista
+ * @param list  struktura list_pointers określająca listę z której użytkownik ma usunąć element
+ * @param order - zmienna order określajaca w jakim kierunku ma się wyświetlać lista
  * @return list_pointers -  struktura list_pointers określająca listę z usuniętymi przez użytkownika elementami
  */
 
@@ -230,12 +230,12 @@ list_pointers delete_element(list_pointers list, int order)
 		else
 			print_list_head(list);
 
-		printf("what is the id of an element  you'd like to remove: \n\n");
+		printf("what is the id of an element you'd like to remove: \n\n");
 		scanf_s("%d", &id, 1);
 		list_node* element_to_del = find_node(list, id);
 		getchar();
 		list=list_remove_node(list, element_to_del);
-		printf("\n\n would you like to delete another element: \n 1- yes \n 0- no \n\n");
+		printf("\n\n Would you like to delete another element: \n 1- yes \n 0- no \n\n");
 		scanf_s("%d", &choice, 1);
 		if (choice == 0)
 			break;
@@ -253,10 +253,10 @@ list_pointers delete_element(list_pointers list, int order)
 /**
  * @brief funkcja generująca interfejs umożliwiający zapisanie listy do pliku
  *
- * na początku funkcja pytanie sie użytkownika o podanie ciągu znaków określającego nazwę pliku pod jaką ma być zapisany
+ * na początku funkcja pyta sie użytkownika o podanie ciągu znaków określającego nazwę pliku pod jaką ma być zapisany,
  * a następnie zapisuje ta liste pod tą nazwę z użyciem funkcji save()
  *
- * @param list  struktura list_pointers określająca listę z którą użytkownik chce zapisać
+ * @param list  struktura list_pointers określająca listę którą użytkownik chce zapisać
  */
 
 void save_phone_book(list_pointers list)
@@ -266,10 +266,10 @@ void save_phone_book(list_pointers list)
 	do
 	{
 		system("cls");
-		printf("how would you like to name your phone_book: \n\n");
+		printf("How would you like to name your phone_book: \n\n");
 		scanf_s("%s", file_name, 30);
 		save(list, file_name);
-		printf("\n\n would you like to save  another copy of your phone book: \n 1- yes \n 0- no \n\n");
+		printf("\n\n Would you like to save another copy of your phone book: \n 1- yes \n 0- no \n\n");
 		scanf_s("%d", &choice, 1);
 		if (choice == 0)
 			break;
@@ -282,10 +282,10 @@ void save_phone_book(list_pointers list)
 
 
 /**
- * @brief funkcja generuje interfejs umożliwiająca edycja konkretnego elementu wpisu w książce telefonicznej
+ * @brief funkcja generuje interfejs umożliwiający edycje konkretnego elementu wpisu w książce telefonicznej
  *
- * najpierw funkcja prosi użytkownika o wybór który element tego wpisu chcę zmienić następnie podaje ciąg znaków który ma nadpisać
- * aktualny stan elementu a następnie nadpisuje ten element w zależności od wyboru
+ * najpierw funkcja prosi użytkownika o wybór który element tego wpisu chcę zmienić, następnie podaje ciąg znaków który ma nadpisać
+ * aktualny stan elementu, a następnie nadpisuje ten element w zależności od wyboru
  *
  *
  * @param element_to_edit - wskaźnik na element który ma być poddany edycji
@@ -299,12 +299,12 @@ void edit_element(list_node* element_to_edit)
 
 		int choice;
 	do {
-		printf("what element would you like to edit: \n\n");
-		printf("7- edit name\n6- edit surname\n5- edit street name\n4- house number\n3- edit postal code\n2- edit city\n1- edit phone number\n0-fisnish editing this contact\n\n ");
+		printf("wWhat element would you like to edit: \n\n");
+		printf("7- edit name\n6- edit surname\n5- edit street name\n4- house number\n3- edit postal code\n2- edit city\n1- edit phone number\n0- finish editing this contact\n\n ");
 		scanf_s("%d", &choice, 1);
 		if (choice == 0)
 			break;
-		printf("\n please provide new value:\n");
+		printf("\n Please provide new value:\n");
 		char new[30];
 			scanf_s("%s", new, 30);
 
@@ -331,12 +331,12 @@ void edit_element(list_node* element_to_edit)
 /**
  * @brief funkcja generująca interfejs wyboru elementu do edycji
  *
- * na początku funkcja prosi użytkownika o podanie id elementu który ma być poddany edycji
- * następnie przeszukuje listę w jego poszukiwaniu, jeżeli nie nie ma elementu o takim id funkcja wyświetla że nie znaleziono takiego
- * element, po znaleziono elementu funkcja aktywuje funkcję edit_element na wybranym elemencie listy. po zakończeniu funkcja zwraca\
+ * na początku funkcja prosi użytkownika o podanie id elementu który ma być poddany edycji,
+ * następnie przeszukuje listę w jego poszukiwaniu, jeżeli nie ma elementu o takim id funkcja wyświetla że nie znaleziono takiego
+ * elementu, po znalezieniu elementu funkcja aktywuje funkcję edit_element na wybranym elemencie listy. Po zakończeniu funkcja zwraca\
  * zedytowana listę
  *
- * @param list  struktura list_pointers określająca listę z którą użytkownik chce zapisać
+ * @param list  struktura list_pointers określająca listę którą użytkownik chce zapisać
  * @return list_pointers  struktura list_pointers określają listę z edytowanymi elementami
  */
 
@@ -347,12 +347,12 @@ list_pointers edit_phone_list(list_pointers list)
 	do
 	{
 		system("cls");
-		printf("what is the id of an element  you'd like to edit: \n\n");
+		printf("What is the id of an element you'd like to edit: \n\n");
 		scanf_s("%d", &id, 1);
 		list_node* element_to_edit = find_node(list, id);
 		if (element_to_edit == NULL)
 		{
-			printf("there is no eelemt with that id\n");
+			printf("There is no eelemt with that id\n");
 			continue;
 
 		}
@@ -363,7 +363,7 @@ list_pointers edit_phone_list(list_pointers list)
 
 
 
-		printf("\n\n would you like to eddit another element: \n 1- yes \n 0- no \n\n");
+		printf("\n\n Would you like to edit another element: \n 1- yes \n 0- no \n\n");
 		scanf_s("%d", &choice, 1);
 		if (choice == 0)
 			break;
@@ -380,7 +380,7 @@ list_pointers edit_phone_list(list_pointers list)
  *
  * funkcja wyświetla użytkownikowi wybór i w zależności od wyboru funkcja może przejść do funkcji interfejsu: save_phone_book, search_phone_book
  * sort_phone_book, edit_phone_list, delete_element, add_new_element.
- * po zakończeniu pracy lista funkcja zwalnia pamięć zajmowana przez listę
+ * po zakończeniu pracy funkcja zwalnia pamięć zajmowana przez listę
  *
  * @param list - struktura list_pointers określająca listę która użytkownik ma poddać edycji
  */
@@ -397,8 +397,8 @@ void phone_book_manage(list_pointers list)
 		else
 			print_list_head(list);
 
-		printf("what would you like to do with your phone list: \n\n");
-		printf("6- add new element\n5- delete element\n4- edit element\n3- sort phone book \n2- search for element\n1- save phone book\n0-fisnish working with this phone book\n\n ");
+		printf("What would you like to do with your phone list: \n\n");
+		printf("6- add new element\n5- delete element\n4- edit element\n3- sort phone book \n2- search for element\n1- save phone book\n0- finish working with this phone book\n\n ");
 		scanf_s("%d", &choice, 1);
 		switch (choice)
 		{
@@ -422,8 +422,8 @@ void phone_book_manage(list_pointers list)
 
 /**
  * @brief funkcja odpowiada za tworzenie nowej listy
- * funkcja torzy nowa psuta listę a następnie prosi użytkownika o podanie elementu z pomocą funkcji add_new_element
- * az użytkownik nie zdecyduje że chce przestać, po zakończeniu w wpisywania przechodzi z ta lista do interfejsu zarządzania
+ * funkcja tworzy nowa pustą listę a następnie prosi użytkownika o podanie elementu z pomocą funkcji add_new_element
+ * az użytkownik nie zdecyduje że chce przestać, po zakończeniu wpisywania przechodzi z ta lista do interfejsu zarządzania
  * phone_book_manage()
  *
  */
@@ -434,9 +434,9 @@ void create_new_phone_book()
 	int choice;
 	list_pointers list = list_init();
 	do{
-		printf("please provide your contact details: \n\n");
+		printf("Please provide your contact details: \n\n");
 		list = add_new_element(list);
-	printf("\n\n would you like to add another element: \n 1- yes \n 0- no \n\n");
+	printf("\n\n Would you like to add another element: \n 1- yes \n 0- no \n\n");
 	scanf_s("%d", &choice, 1);
 	if (choice == 0)
 		break;
@@ -456,8 +456,8 @@ void create_new_phone_book()
 /**
  * @brief funkcja odpowiada za segment menu odpowiedzialny za otwarcie książki z pliku
  *
- * funkcja wyświetla uztkonkowi listę książek dostępnych w dedykowanym folderze
- * a następnie pozwala użytkownikowi numer pliku który chce otworzyć'
+ * funkcja wyświetla użytkownikowi listę książek dostępnych w dedykowanym folderze
+ * a następnie pozwala użytkownikowi wybrac numer pliku który chce otworzyć,
  * następnie na podstawie wybranego numeru ładuje plik do nowej listy i przechodzi z ta lista do interfejsu zarządzania
  * phone_book_manage()
  */
@@ -467,12 +467,12 @@ void opening_book()
 	int choice;
 	if (!phone_book_list())
 		return;
-	printf("\n\n please choose number of phone you would like to open: \n");
+	printf("\n\n Please choose number of phone book you would like to open: \n");
 	scanf_s("%d", &choice, 1);
 	char file_name[30] = "";
 	if (!get_file_name(file_name, choice))
 	{
-		printf("there is phone book with that number");
+		printf("There is phone book with that number");
 		return NULL;
 	}
 	printf("%s", file_name);
@@ -496,7 +496,7 @@ void start_ui()
 	int choice=0;
 	do{
 		system("cls");
-		printf("\nwhat would you like to do: \n\n2- open phone_book \n1- create_new\n0- close software:\n\n ");
+		printf("\nWhat would you like to do: \n\n2- open phone_book \n1- create_new\n0- close software:\n\n ");
 		scanf_s("%d", &choice, 1);
 		switch (choice)
 		{
